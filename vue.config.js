f07eed67c063266aaa3b9f911fedef6a005d60c1
zhiyong-y <15684187702@163.com>
2021-09-12 15:40:41 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ module.exports = {
   // assetsPublicPath: './',
   // 设置跟路径
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/sog/' : '',
+    process.env.NODE_ENV === 'production' ? '/sog/' : '/',
   // outputDir: "",
   // assetsDir: 'assets',
   // pages: {},
+  indexPath: 'index.html',
   lintOnSave: true,
   // 解决安装 less 以及 less-loader 后报错，JavaScript 不被允许使用
   css: {
